d02193076f1bd61e08082ef443c6be71265b0fea
markmia14 <63660531+markmia14@users.noreply.github.com>
2020-05-21 22:04:17 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// question 1
+
+// answer for q1
+var firstVar;
+
+
+// question 2
+
+// answer for q2
+var name = "Mark";
+console.log(name);
+
+
+// question 3 
+
+// answer for q3
+var birthDate = 23;
+console.log(birthDate);
+
+// question 4
+
+// answer for q4
+var division = 20 / 5;
+console.log(division);
+
+// question 5
+
+// answer for q5
+var animal1 = "horse", animal2 = "frog";
+console.log(animal1, animal2);
+animal1 === animal1
+animal1 === animal2
+animal2 === animal2
+
+// question 6
+
+// answer for q6
+var orderHasShipped = true;
+console.log(orderHasShipped);
+
+// question 7
+
+// answer for q7
+var orderHasShipped = window.confirm ("select Ok for true, select cancel for false");
+if (orderHasShipped === true){
+console.log("true");
+} else {
+    console.log("false");
+} 
+
+// question 8 
+
+// answer for q8
+for (var count = 0; count < 10; count++){
+    console.log(count);
+}
+
+
+
+
+
+
+
+
+
+
+
+
